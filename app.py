359b2e9dd2f39e9c4ba1b573c6594c813adfee3f
rckmnds <81979401+rckmnds@users.noreply.github.com>
2022-05-25 20:09:00 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -9,15 +9,5 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Mais um Hello World \o/"
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
 if __name__ == '__main__':
     app.run()
